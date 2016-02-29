c0833b456026a6ef944d26d19ca4ab104fdf0f37
Iván Manso <ivan.manso@babel-si.local>
2016-02-29 12:23:31 +0100

    Preparación del formulario en html

--- js/src/app.js
+++ js/src/app.js
@@ -1,27 +1,61 @@
 $(document).ready(function(){//Cuando la página se ha cargado por completo
 
-	$(".auto-focus").focus();//ponemos el foco en el primer input
+	$(".artist").focus();//ponemos el foco en el primer input
 
 	$("form").on("submit", function(){ //cuando se intente enviar
-		var title = $.trim($("#title").val());
+		var artist = $.trim($("#artist").val());
+		var title =  $.trim($("#song").val());
+
+		if(artist == ""){ //validación del artista
+			alert("El título no puede ser vacío");
+			return false;
+		}
+
 		if(title == ""){ //validación del título
 			alert("El título no puede ser vacío");
 			return false;
 		}
 
-		var url = $.trim($("#cover_url").val());
+		var url_audio = $.trim($("#audio").val());
+		var url_image = $.trim($("#imagen").val());
+
 		var pattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/ig;
 
-		if( url !="" && pattern.test(url) == false){
+		if( url_audio !="" && pattern.test(url_audio) == false){ //validación de la url de la canción
 			alert("La url no es válida");
 			return false;
 		}
 
-		//validación de categorías
-		var selectedCategories = $('input[name="category"]:checked');
-		if(selectedCategories.length == 0){
-			alert("Selecciona al menos una categoría");
+		if( url_image !="" && pattern.test(url_image) == false){ //validación de la url de la imagen
+			alert("La url no es válida");
+			return false;
 		}
+
+
+		$("#save").on("click", function(){
+			$.ajax({ //preparación de la petición de añadir canción
+			method: 'post',
+			url: "/api/playlist",
+			data: JSON.stringify({
+				title: title,
+				url: url
+			}),
+			contentType: 'application/json',
+			success: function(){
+				reloadSeries();
+				alert("Guardado con éxito");
+			},
+			error: function(){
+				alert("Se ha producido un error");
+			}
+		});
+		})
+
+
+
+
+
+
 
 		$.ajax({ //preparación de la petición que llegará de manera asíncrona
 			method: 'post',
